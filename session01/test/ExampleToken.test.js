da3fc53c9c8a67461f2c6b02d68a287ea2b64eae
Jasper Lee <huna3869@hanyang.ac.kr>
2022-07-29 18:30:02 +0900

    chore: add test

--- session01/test/ExampleToken.test.js
+++ session01/test/ExampleToken.test.js
@@ -1,15 +1,44 @@
 const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
 
 describe("ExampleToken", function () {
   let alice, bob;
-  let token;
-  
-  it("constructor() should assign the total supply of tokens to the deployer", async function () {});
-  it("transfer() should revert when balance is smaller than transfer amount", async function () { });
-  it("mint() should emit Transfer event", async function () { });
-  it("transfer() should return true when transfer done without revert", async function () {});
-  it("approve() should change allowance", async function () {});
-  it("transferFrom() should fail when allowance is smaller than transfer amount", async function () {});
-  it("transferFrom() should return true when transfer done without revert", async function () {});
-  it("burn() should emit Transfer event", async function () { });
+  let token, tokenFactory;
+
+  beforeEach(async function () {
+    [alice, bob] = await ethers.getSigners();
+    tokenFactory = await ethers.getContractFactory("ExampleToken");
+    token = await tokenFactory.connect(alice).deploy("A Token", "ATT");
+  })
+
+  it("constructor() should assign the total supply of tokens to the deployer", async function () {
+    deployerBalance = await token.balanceOf(alice.address);
+    expect(await token.totalSupply()).to.equal(deployerBalance);
+  });
+  it("transfer() should revert when balance is smaller than transfer amount", async function () {
+    expect(token.connect(bob).transfer(alice.address, 100)).to.be.reverted;
+  });
+  it("mint() should emit Transfer event", async function () {
+    expect(await token.connect(alice).mint(100)).to.emit(token, 'Transfer').withArgs(ethers.constants.AddressZero, alice.address, 100);
+  });
+  it("transfer() should return true when transfer done without revert", async function () {
+    let ret = await token.connect(alice).callStatic.transfer(bob.address, 100);
+    expect(ret).to.be.equal(true);
+  });
+  it("approve() should change allowance", async function () {
+    await token.connect(bob).approve(alice.address, 100);
+    expect(await token.allowance(bob.address, alice.address)).to.be.equal(100);
+  });
+  it("transferFrom() should fail when allowance is smaller than transfer amount", async function () {
+    expect(token.connect(bob).transferFrom(alice.address, bob.address, 100)).to.be.reverted;
+  });
+  it("transferFrom() should return true when transfer done without revert", async function () {
+    await token.connect(alice).approve(bob.address, 100);
+    let ret = await token.connect(bob).callStatic.transferFrom(alice.address, bob.address, 100);
+    expect(ret).to.be.equal(true);
+  });
+  it("burn() should emit Transfer event", async function () {
+    expect(await token.connect(alice).burn(100)).to.emit(token, 'Transfer').withArgs(alice.address, ethers.constants.AddressZero, 100);
+  });
 });
--- session01/test/SimpleSwap.test.js
+++ session01/test/SimpleSwap.test.js
@@ -1,13 +1,49 @@
 const { expect } = require("chai");
 
 describe("SimpleSwap", function () {
-  let tokenA, tokenB, tokenSwap;
+  let tokenA, tokenB, tokenM, simpleSwap;
   let alice, bob, mallory;
-  beforeEach(async function () {})
-  it("swap() should revert if caller is not owner1 and owner2", async function () { });
-  it("swap() should revert when owner1 allowance is smaller than swap amount", async function () { });
-  it("swap() should revert when owner2 balance is smaller than swap amount", async function () { });
-  it("swap() should revert if tokenA is not ERC20", async function () { });
-  it("swap() should tranfer swap amount to each owners", async function () { });
-  it("swap() might not work properly if token is malicious", async function () { });
+  beforeEach(async function () {
+    [alice, bob, mallory] = await ethers.getSigners();
+    tokenFactory = await ethers.getContractFactory("ExampleToken");
+    malTokenFactory = await ethers.getContractFactory("MalToken");
+    swapFactory = await ethers.getContractFactory("SimpleSwap");
+    tokenA = await tokenFactory.connect(alice).deploy("A Token", "ATT");
+    tokenB = await tokenFactory.connect(bob).deploy("B Token", "BTT");
+    tokenM = await malTokenFactory.connect(mallory).deploy("Mal Token", "MTT");
+    simpleSwap = await swapFactory.connect(bob).deploy(tokenA.address, alice.address, 100, tokenB.address, bob.address, 20);
+  })
+  it("swap() should revert if caller is not owner1 and owner2", async function () {
+    expect(simpleSwap.connect(mallory).swap()).to.be.revertedWith("Not authorized")
+  });
+  it("swap() should revert when owner1 allowance is smaller than swap amount", async function () {
+    expect(simpleSwap.connect(alice).swap()).to.be.revertedWith("Token 1 allowance too low")
+  });
+  it("swap() should revert when owner2 balance is smaller than swap amount", async function () {
+    await tokenA.connect(alice).approve(simpleSwap.address, 100);
+    await tokenB.connect(bob).approve(simpleSwap.address, 20);
+    await tokenB.connect(bob).burn(await tokenB.totalSupply());
+    expect(simpleSwap.connect(alice).swap()).to.be.reverted;
+  });
+  it("swap() should revert if tokenA is not ERC20", async function () {
+    simpleSwap = await swapFactory.connect(bob).deploy(mallory.address, alice.address, 100, tokenB.address, bob.address, 20);
+    await tokenA.connect(alice).approve(simpleSwap.address, 100);
+    await tokenB.connect(bob).approve(simpleSwap.address, 20);
+    expect(simpleSwap.connect(alice).swap()).to.be.reverted;
+  });
+  it("swap() should tranfer swap amount to each owners", async function () {
+    await tokenA.connect(alice).approve(simpleSwap.address, 100);
+    await tokenB.connect(bob).approve(simpleSwap.address, 20);
+    await simpleSwap.connect(alice).swap();
+    expect(await tokenB.balanceOf(alice.address)).to.be.equal(20);
+    expect(await tokenA.balanceOf(bob.address)).to.be.equal(100);
+  });
+  it("swap() might not work properly if token is malicious", async function () {
+    simpleSwap = await swapFactory.connect(bob).deploy(tokenM.address, mallory.address, 10, tokenB.address, bob.address, 100000);
+    await tokenM.connect(mallory).approve(simpleSwap.address, 10);
+    await tokenB.connect(bob).approve(simpleSwap.address, 100000);
+    await simpleSwap.connect(mallory).swap();
+    expect(await tokenB.balanceOf(mallory.address)).to.be.equal(100000);
+    expect(await tokenM.balanceOf(bob.address)).to.be.equal(0);
+  });
 });
